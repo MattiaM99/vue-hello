89e308638e65cd202f6f76f764643609afaa158a
MattiaM99 <mattiamazzoni123@gmail.com>
2021-11-10 16:02:46 +0100

    finito bonus

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 new Vue({
   el: "#app",
   data: {
-    msg: "Hello World!"
+    msg: "Hello World!",
+    myImage:''
+  },
+  methods:{
+    myFunction: function () {	
+		this.myImage = "../img/1_nq9cdMxtdhQ0ZGL8OuSCUQ.jpg"
+    }
   }
 });
